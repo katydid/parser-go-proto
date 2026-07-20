e9d7166a8c21a2b0bab391f5ec0e4e98e91aadd6
Walter Schulze <awalterschulze@gmail.com>
2026-07-20 07:31:00 +0100

    separate proto.go into 3 files

--- proto/proto.go
+++ proto/proto.go
@@ -31,56 +31,6 @@ import (
 	descriptor "google.golang.org/protobuf/types/descriptorpb"
 )
 
-type errVarint struct {
-	buf []byte
-}
-
-func newErrVarint(buf []byte) error {
-	buffer := make([]byte, 10)
-	nn := copy(buffer, buf)
-	return &errVarint{buffer[:nn]}
-}
-
-func (this *errVarint) Error() string {
-	return fmt.Sprintf("parser/proto: error decoding varint from %#v", this.buf)
-}
-
-var errBufferOverlow = fmt.Errorf("buffer overflow")
-
-var errUnknownWireType = fmt.Errorf("unknown wire type")
-
-func uvarint(buf []byte) (uint64, int, error) {
-	var uv uint64
-	var shift uint
-	for i, b := range buf {
-		if b < 0x80 {
-			if i > 9 || i == 9 && b > 1 {
-				return 0, 0, newErrVarint(buf)
-			}
-			return uv | uint64(b)<<shift, i + 1, nil
-		}
-		uv |= (uint64(b) & 0x7F) << shift
-		shift += 7
-	}
-	return 0, 0, newErrVarint(buf)
-}
-
-func length(wireType int, buf []byte) (prefix int, l int, err error) {
-	switch wireType {
-	case 0:
-		_, n2, err := uvarint(buf)
-		return 0, n2, err
-	case 1:
-		return 0, 8, nil
-	case 2:
-		l, n2, err := uvarint(buf)
-		return n2, int(l), err
-	case 5:
-		return 0, 4, nil
-	}
-	return 0, 0, errUnknownWireType
-}
-
 type protoParser struct {
 	descMap DescMap
 	state
